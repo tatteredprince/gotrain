3a30f510d86e615687695fd502ff7c34623587cd
Tiserion Celtaris <tatteredprince@github.com>
2026-04-20 22:22:29 +0300

    add solution

--- add_two_numbers_test.go
+++ add_two_numbers_test.go
@@ -14,17 +14,20 @@ func addTwoNumbers(a, b []int) []int {
 func addTwoNumbersTestHelper(t *testing.T, a, b, expect []int) {
 	t.Helper()
 	t.Logf("add numbers %v and %v to get %v", a, b, expect)
-	if got := addTwoNumbers(); !slices.Equal(got, expect) {
+	if got := addTwoNumbers(a, b); !slices.Equal(got, expect) {
 		t.Fatalf("got %v but expected %v", got, expect)
 	}
 }
 
 func TestAddTwoNumbers(t *testing.T) {
 	t.Run("Two zero numbers", func(t *testing.T) { addTwoNumbersTestHelper(t, []int{0}, []int{0}, []int{0}) })
+	t.Run("Two equal numbers", func(t *testing.T) {
+		addTwoNumbersTestHelper(t, []int{5, 5}, []int{5, 5}, []int{0, 1, 1})
+	})
 	t.Run("Three-digit and two-digit", func(t *testing.T) {
-		addTwoNumbersTestHelper(t, []int{3, 2, 1}, []int{8, 7}, []int{0, 1, 2})
+		addTwoNumbersTestHelper(t, []int{3, 2, 1}, []int{7, 8}, []int{0, 1, 2})
 	})
 	t.Run("Two three-digit numbers", func(t *testing.T) {
-		addTwoNumbersTestHelper(t, []int{2, 4, 3}, []int{4, 6, 5}, []int{7, 0, 8})
+		addTwoNumbersTestHelper(t, []int{2, 4, 3}, []int{5, 6, 4}, []int{7, 0, 8})
 	})
 }
