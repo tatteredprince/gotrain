a67362f0689a161bee6476d1d287c9524c583d87
Tiserion Celtaris <tatteredprince@github.com>
2026-04-20 22:23:39 +0300

    add solution

--- add_two_numbers_test.go
+++ add_two_numbers_test.go
@@ -8,6 +8,40 @@ import (
 // addTowNumbers returns sum of two non-negative numbers, all three written backwards
 func addTwoNumbers(a, b []int) []int {
 	sum := make([]int, 0)
+	var (
+		longest  []int
+		shortest []int
+	)
+	if len(a) > len(b) {
+		longest, shortest = a, b
+	} else {
+		longest, shortest = b, a
+	}
+	addendum := 0
+	i := 0
+	for ; i < len(shortest); i++ {
+		_sum := shortest[i] + longest[i] + addendum
+		if _sum >= 10 {
+			_sum -= 10
+			addendum = 1
+		} else {
+			addendum = 0
+		}
+		sum = append(sum, _sum)
+	}
+	for ; i < len(longest); i++ {
+		_sum := longest[i] + addendum
+		if _sum >= 10 {
+			_sum -= 10
+			addendum = 1
+		} else {
+			addendum = 0
+		}
+		sum = append(sum, _sum)
+	}
+	if addendum != 0 {
+		sum = append(sum, addendum)
+	}
 	return sum
 }
 
